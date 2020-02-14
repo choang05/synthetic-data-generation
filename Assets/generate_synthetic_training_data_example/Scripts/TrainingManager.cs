881a66ce05648ebdadd78c5a9ac90316f126db55
JW Banks <jwbanks55@gmail.com>
2020-02-13 18:21:50 -0600

    NuGit added to project

---
+++ Assets/generate_synthetic_training_data_example/Scripts/TrainingManager.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingManager : MonoBehaviour
+{
+    CustomVisionTrainingClient client;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        client = new CustomVisionTrainingClient();
+        client.GetProject();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
