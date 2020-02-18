56e4697b7fdd1f3d36e4e295d3d4a78f47cc5609
DanielValdes <monosaicol@gmail.com>
2020-02-17 19:49:21 -0600

    ui initialized
    Adding in the UI canvases, scenes and images of everything that's been done so far. Up to this point we have build7.apk on elsy's phone.

---
+++ XRShowDown_Mobile/Assets/UI/Scripts/PictureTaker.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PictureTaker : MonoBehaviour
+{
+
+    public int num = 0;
+    private WebCamDevice cameraDevice;
+    private WebCamTexture camera;
+
+    public RawImage DefaultBackground;
+
+    public Text myText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+
+        if (devices.Length == 0)
+        {
+            Debug.Log("no Cameras found");
+            myText.text = "No Cameras found";
+
+        }
+        else
+            myText.text = "we got some Cameras!  " + devices.Length;
+
+        for (int i = 0; i < devices.Length; i++)
+        {
+
+            if (!devices[i].isFrontFacing)
+            {
+                camera = new WebCamTexture(devices[i].name, Screen.height, Screen.width);
+            }
+
+            if(camera == null)
+            {
+                myText.text = " we could not find back Camera";
+            }
+
+
+          
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+   
+
+
+    public void takePic()
+    {
+        num = num +1;   //   num++;
+        Debug.Log("You took " + num + "  pic");
+
+        camera.Play();
+        DefaultBackground.texture = camera;
+
+    }
+        
+}
