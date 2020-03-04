71b5fb785d82234d649189d69d40f405873614d7
choang05@gmail.com <choang05@gmail.com>
2020-03-03 19:53:58 -0600

    Added first picture take text tutortial and logic to remove it when first image is loaded.

---
+++ XRShowDown_Mobile/Assets/Scripts/OnFirstPictureTaken.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnFirstPictureTaken : MonoBehaviour
+{
+    public GameObject objToSetActive;
+    public bool setActiveOnFirstPictureLoaded = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        PictureTaker.OnPictureLoaded += OnPictureLoaded;  
+    }
+
+    private void OnDisable()
+    {
+        PictureTaker.OnPictureLoaded -= OnPictureLoaded;
+    }
+
+    private void OnPictureLoaded(Texture2D texture2d, RawImage rawImage, string path)
+    {
+        if (objToSetActive)
+        {
+            objToSetActive.SetActive(setActiveOnFirstPictureLoaded);
+        }
+        else
+        {
+            gameObject.SetActive(setActiveOnFirstPictureLoaded);
+        }
+    }
+}
