6f1c5dff2575de8e7bb25ff2a3de6969ab0a1a32
DanielValdes <monosaicol@gmail.com>
2020-03-05 20:10:52 -0600

    enable and disable debugger Text

--- XRShowDown_Mobile/Assets/Scripts/FPSDisplay.cs
+++ XRShowDown_Mobile/Assets/Scripts/FPSDisplay.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class FPSDisplay : MonoBehaviour
 {
 	float deltaTime = 0.0f;
 
-
+	public Text fpsText;
 	private void Start()
 	{
 		DontDestroyOnLoad(this.gameObject);
@@ -13,21 +14,8 @@ public class FPSDisplay : MonoBehaviour
 	void Update()
 	{
 		deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
-	}
-
-	void OnGUI()
-	{
-		int w = Screen.width, h = Screen.height;
-
-		GUIStyle style = new GUIStyle();
-
-		Rect rect = new Rect(0, 0, w, h * 2 / 25);
-		style.alignment = TextAnchor.LowerRight;
-		style.fontSize = h * 4 / 100;
-		style.normal.textColor = new Color(1, 0.0f, 0f, 1.0f);
 		float msec = deltaTime * 1000.0f;
 		float fps = 1.0f / deltaTime;
-		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
-		GUI.Label(rect, text, style);
+		fpsText.text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
 	}
 }
